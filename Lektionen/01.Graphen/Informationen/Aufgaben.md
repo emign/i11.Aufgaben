4023d71cab46e9e3dc61025822f92ad798bf5c02
Nicolai Emig <nicolai.emig@amseegym.de>
2021-03-02 11:02:25 +0100

    🔥 Initial Commit

---
+++ Lektionen/01.Graphen/Informationen/Aufgaben.md
@@ -0,0 +1,4 @@
+# 1. Graphen
+## Aufgabe 1
+Implementieren Sie einen VerweisGraph nach dem folgenden Klassendiagramm
+![Bild](Klassendiagramm.png)
---
+++ Lektionen/01.Graphen/build.gradle.kts
@@ -0,0 +1,9 @@
+import com.soywiz.korge.gradle.*
+
+apply<KorgeGradlePlugin>()
+
+korge {
+    id = "me.emig.lektion.01.Graphen"
+    targetJvm()
+    targetJs()
+}
